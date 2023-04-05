0ed8377590dd9dfc96904f47dbb44083b704b6e0
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2023-04-05 12:00:01 -0700

    Creation of sub dataframe

--- code/Initial_processing_code.R
+++ code/Initial_processing_code.R
@@ -6,8 +6,19 @@
 lapply(c("plyr","dplyr","ggplot2","cowplot",
          "lubridate","tidyverse", "reshape2"), require, character.only=T)
 
+## Check working directory so that you are in setwd("~/GitHub/CyanoSpatial/code")
+getwd()
+
 ## Read in data
+data <- read.csv("../data/SWAMP_cyanotoxins_water.csv", header=T)
 
+## Become familiar with data set
+colnames(data)
 
+sub <- data[,c("StationName","StationCode","SampleDate","CollectionTime",
+               "Analyte","Unit","Result","MDL","CollectionMethodName",
+               "TargetLatitude","TargetLongitude")]
 
-
+## Goal: try and summarize results by StationName
+# organize by analyte type and station - how many samples do we have for different analytes from different locations
+# create a table
